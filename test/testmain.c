80bf68e0f06d059b421b130eeb9f54954bb10828
tripcodeuser <anon@nowhere>
2011-06-22 21:58:10 +0300

    add valgrind assist #if clause to testmain.c

--- test/testmain.c
+++ test/testmain.c
@@ -13,6 +13,10 @@ int main(void)
 	g_type_init();
 
 	SRunner *sr = srunner_create(pt_cache_suite());
+#if 0
+	/* for valgrinding */
+	srunner_set_fork_status(sr, CK_NOFORK);
+#endif
 	srunner_run_all(sr, CK_NORMAL);
 	int nf = srunner_ntests_failed(sr);
 	srunner_free(sr);
